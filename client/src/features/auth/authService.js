f32df96471cf8d9355c67035af2f307a1bde2ec3
Harshadkumawat <harshadkumawat339977@gmail.com>
2025-10-09 00:57:06 +0530

    url chnage

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,41 +1,37 @@
-const api = "http://localhost:8080";
-
 import axios from "axios";
-import { fetchCurrentUser } from "./authSlice";
 
-axios.defaults.withCredentials = true;
+const API_URL = "https://krume-ku.onrender.com"; 
+
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 // REGISTER
 const authRegister = async (data) => {
-  const response = await axios.post(`${api}/api/auth/register`, data, {
-    headers: { "Content-Type": "application/json" },
-  });
+  const response = await api.post("/api/auth/register", data);
   return response.data;
 };
 
 // LOGIN
 const authLogin = async (data) => {
-  const response = await axios.post(`${api}/api/auth/login`, data, {
-    headers: { "Content-Type": "application/json" },
-  });
+  const response = await api.post("/api/auth/login", data);
   return response.data;
 };
 
 // LOGOUT
 const authLogout = async () => {
-  const response = await axios.post(
-    `${api}/api/auth/logout`,
-    {},
-    { headers: { "Content-Type": "application/json" } }
-  );
+  const response = await api.post("/api/auth/logout", {});
   return response.data;
 };
 
+// CURRENT USER
 const getCurrentUser = async () => {
-  const response = await axios.get(`${api}/api/auth/me`, {
-    headers: { "Content-Type": "application/json" },
-  });
-
+  const response = await api.get("/api/auth/me");
   return response.data;
 };
 
