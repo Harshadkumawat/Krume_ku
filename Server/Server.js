72ba9b9e52bd61f114c855cf317b1edd38d55300
Harshadkumawat <harshadkumawat339977@gmail.com>
2025-10-09 01:08:21 +0530

    server url chnage

--- Server/Server.js
+++ Server/Server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://krume-ku.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
